4a1f499ca13af57052a29306e7c3e59be175fd59
Andrew <andsof123455@gmail.com>
2021-05-02 00:11:59 +0300

    Add property preview

--- Housing/src/app/property/add-property/add-property.component.ts
+++ Housing/src/app/property/add-property/add-property.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { TabsetComponent } from 'ngx-bootstrap/tabs';
+import { IProperty } from '../IProperty.interface';
 
 @Component({
   selector: 'app-add-property',
@@ -17,9 +18,18 @@ export class AddPropertyComponent implements OnInit {
   propertyTypes: Array<string> = ['House', 'Apartment', 'Duplex'];
   furnishTypes: Array<string> = ['Fully', 'Semi', 'Unfurnished'];
 
+  propertyView: IProperty = {
+    Id: null,
+    Name: '',
+    Price: null,
+    SellRent: null,
+    Type: null
+  };
+
   constructor(private _router: Router) { }
 
   ngOnInit() {
+
   }
 
   onBack() {
