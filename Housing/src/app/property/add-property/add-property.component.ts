de377b2f7db75a879ba261add156d019f7033589
Andrew <andsof123455@gmail.com>
2021-05-01 23:00:56 +0300

    Next button functionality

--- Housing/src/app/property/add-property/add-property.component.ts
+++ Housing/src/app/property/add-property/add-property.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { TabsetComponent } from 'ngx-bootstrap/tabs';
 
 @Component({
   selector: 'app-add-property',
@@ -10,6 +11,7 @@ import { Router } from '@angular/router';
 export class AddPropertyComponent implements OnInit {
 
   @ViewChild('Form') addPropertyForm: NgForm;
+  @ViewChild('formTabs') formTabs: TabsetComponent;
 
   constructor(private _router: Router) { }
 
@@ -23,5 +25,9 @@ export class AddPropertyComponent implements OnInit {
   onSubmit() {
     console.log('Congrats');
     console.log(this.addPropertyForm);
+  }
+
+  selectTab(tabId: number) {
+    this.formTabs.tabs[tabId].active = true;
   }
 }
