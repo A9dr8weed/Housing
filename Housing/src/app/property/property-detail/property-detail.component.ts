8a840c48d80d28cbd3fc9470e460842f274a9a60
Andrew <andsof123455@gmail.com>
2021-05-06 17:40:44 +0300

    Implement ngx-gallery for photos

--- Housing/src/app/property/property-detail/property-detail.component.ts
+++ Housing/src/app/property/property-detail/property-detail.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Property } from 'src/app/model/Property';
 import { HousingService } from 'src/app/services/housing.service';
+import { NgxGalleryOptions } from '@kolkov/ngx-gallery';
+import { NgxGalleryImage } from '@kolkov/ngx-gallery';
+import { NgxGalleryAnimation } from '@kolkov/ngx-gallery';
 
 @Component({
   selector: 'app-property-detail',
@@ -11,6 +14,9 @@ import { HousingService } from 'src/app/services/housing.service';
 export class PropertyDetailComponent implements OnInit {
   public propertyId: number;
   property = new Property();
+
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
 
   constructor(
     private _route: ActivatedRoute,
@@ -24,5 +30,43 @@ export class PropertyDetailComponent implements OnInit {
     this._route.data.subscribe((data: Property) => {
       this.property = data['prp'];
     });
+
+    this.galleryOptions = [
+      {
+        width: '100%',
+        height: '465px',
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: true,
+      },
+    ];
+
+    this.galleryImages = [
+      {
+        small: 'assets/images/gallery/inner-1.jfif',
+        medium: 'assets/images/gallery/inner-1.jfif',
+        big: 'assets/images/gallery/inner-1.jfif',
+      },
+      {
+        small: 'assets/images/gallery/inner-2.jfif',
+        medium: 'assets/images/gallery/inner-2.jfif',
+        big: 'assets/images/gallery/inner-2.jfif',
+      },
+      {
+        small: 'assets/images/gallery/inner-3.jfif',
+        medium: 'assets/images/gallery/inner-3.jfif',
+        big: 'assets/images/gallery/inner-3.jfif',
+      },
+      {
+        small: 'assets/images/gallery/inner-4.jfif',
+        medium: 'assets/images/gallery/inner-4.jfif',
+        big: 'assets/images/gallery/inner-4.jfif',
+      },
+      {
+        small: 'assets/images/gallery/inner-5.jfif',
+        medium: 'assets/images/gallery/inner-5.jfif',
+        big: 'assets/images/gallery/inner-5.jfif',
+      },
+    ];
   }
 }
